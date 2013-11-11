a25f0067c97b51d1b7fe27d9282af2adf7e0d49c
vladsaveliev <vladsaveliev@me.com>
2013-11-11 13:43:47 +0400

    Models, first view and tmplate

--- devdays/devdays/urls.py
+++ devdays/devdays/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'devdays.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^/?$', 'devdays_app.views.index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
--- devdays/devdays_app/models.py
+++ devdays/devdays_app/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
 
+class Comment(models.Model):
+    text = models.CharField(max_length=20000, blank=True, null=True)
+
+
 class Group(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=1024)
     year = models.IntegerField()
 
 
 class Student(models.Model):
-    name = models.CharField()
-    surname = models.CharField(blank=True, null=True)
+    name = models.CharField(max_length=1024)
+    surname = models.CharField(max_length=1024, blank=True, null=True)
     group = models.ForeignKey(Group, blank=True, null=True)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=1024)
+    description = models.CharField(max_length=30000, blank=True, null=True)
+    students = models.ManyToManyField(Student)
+    comments = models.ManyToManyField(Comment)
 
 
 class Event(models.Model):
     date = models.DateTimeField(blank=True, null=True)
     projects = models.ManyToManyField(Project)
-
-
-class Project(models.Model):
-    name = models.CharField()
-    description = models.CharField(blank=True, null=True)
-    students = models.ManyToManyField(Student)
-    comments = models.ManyToManyField(Comment)
-
-
-class Comment(models.Model):
-    text = models.CharField(blank=True, null=True)
