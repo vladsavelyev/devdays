395cbfaddface565fef7d23c2ba07380fdd0c713
jonyrock <ilms@live.ru>
2013-11-11 16:54:56 +0400

    stash

--- devdays/devdays_app/projects/__init__.py
+++ devdays/devdays_app/projects/__init__.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render_to_response
+from devdays.devdays_app.models import Project
+
 
 def add(request):
     dict = {'message': 'hello'}
+    p = Project()
+
     return render_to_response('projects/index.html', dict)
