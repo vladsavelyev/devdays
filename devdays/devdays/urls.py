6d7b0aef7849dd08291b18b308e2917973a156c8
Na88ie <nast.mois@gmail.com>
2013-11-11 19:55:48 +0400

    отрдактировала функции для отображения идей и проектов

--- devdays/devdays/urls.py
+++ devdays/devdays/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns(
     url(r'^openid/', include('django_openid_auth.urls')),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
     url(r'^projects', 'devdays_app.views.projects'),
-    url(r'^topics', 'devdays_app.views.topics'),
+    url(r'^ideas', 'devdays_app.views.ideas'),
     url(r'^projects/new', 'devdays_app.views.projects'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- devdays/devdays_app/views.py
+++ devdays/devdays_app/views.py
@@ -9,35 +9,15 @@ def index(request):
 
 
 def projects(request):
-    """
-    предполагается, что projects это последовательность объектов содержащих поля:
-     topic (идея на которой основан проект - foreign key из табицы идей),
-     head (руководитель, по умолчанию автор идеи),
-     entry (список участников).
-    """
     name_title = u'Проекты'
-    items = projects
-    return render_to_response('topics.html', locals())
-
-
-def topics(request):
-    """
-    предполагается, что topics это последовательность объектов содержащих поля:
-     heading (название),
-     description (описание),
-     autor (автор).
-    """
-    name_title = u'Идеи'
-    items = topics
-    return render_to_response('topics.html', locals())
+    projects = Project.objects.all()
+    return render_to_response('projects.html', locals())
 
 
 def ideas(request):
     if request.method == 'GET':
-        ideas = Idea.objects.all()
-
         return render_to_response('ideas.html', {
-            'ideas': [i.name for i in ideas]
+            'ideas': Idea.objects.all()
         })
     else:
         raise Exception('Creating not implemented')
