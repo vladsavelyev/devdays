c4c5e70abff3b1641bbde81204d6a9d89061c942
vladsaveliev <vladsaveliev@me.com>
2013-11-11 13:31:22 +0400

    Models, first template

--- devdays/devdays_app/models.py
+++ devdays/devdays_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField()
+    year = models.IntegerField()
+
+
+class Student(models.Model):
+    name = models.CharField()
+    surname = models.CharField(blank=True, null=True)
+    group = models.ForeignKey(Group, blank=True, null=True)
+
+
+class Event(models.Model):
+    date = models.DateTimeField(blank=True, null=True)
+    projects = models.ManyToManyField(Project)
+
+
+class Project(models.Model):
+    name = models.CharField()
+    description = models.CharField(blank=True, null=True)
+    students = models.ManyToManyField(Student)
+    comments = models.ManyToManyField(Comment)
+
+
+class Comment(models.Model):
+    text = models.CharField(blank=True, null=True)
