5adefc3f88d2fa42f91b11136309602a73189b47
jonyrock <ilms@live.ru>
2013-11-12 01:39:56 +0400

    #20 fix events url

--- devdays/devdays_app/projects/urls.py
+++ devdays/devdays_app/projects/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
+EVENT_PATTERN = r'(?P<event>\d{2}_\d{4})' 
+
 urlpatterns = patterns(
     'devdays_app.projects.views',
-    url(r'^(?P<event>\d{4}_\d)/', 'index'),
-    url(r'new', 'index'),
+    url(EVENT_PATTERN + '/', 'index'),
+    url(EVENT_PATTERN + '/new', 'index'),
 )
