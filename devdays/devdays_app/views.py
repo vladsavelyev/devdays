a96d17d0d76c8a5e4b877d491f9c3d19689169e7
jonyrock <ilms@live.ru>
2013-11-11 21:13:55 +0400

    projects render begin

--- devdays/devdays_app/views.py
+++ devdays/devdays_app/views.py
@@ -8,24 +8,6 @@ def index(request):
     return render_to_response('index.html')
 
 
-def projects(request):
-    name_title = u'Проекты'
-    projects = Project.objects.all()
-    return render_to_response('projects.html', locals())
-
-
-def topics(request):
-    """
-    предполагается, что topics это последовательность объектов содержащих поля:
-     heading (название),
-     description (описание),
-     autor (автор).
-    """
-    name_title = u'Идеи'
-    items = topics
-    return render_to_response('topics.html', locals())
-
-
 def ideas(request):
     if request.method == 'GET':
         return render_to_response('ideas.html', {
