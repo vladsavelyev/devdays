f1928662318b15eeb1d21390779cc4837114dcf7
Na88ie <nast.mois@gmail.com>
2013-11-12 13:17:44 +0400

    добавлена форма для заявки

--- devdays/devdays_app/projects/forms.py
+++ devdays/devdays_app/projects/forms.py
@@ -2,9 +2,9 @@
 __author__ = 'annie'
 from django.forms import forms, CharField, URLField, Textarea
 
-
 class CreateIdea(forms.Form):
     name = CharField()
     description = CharField(widget=Textarea, initial=u'Введите описание')
     autor = CharField()
     link = URLField(required=False)
+
