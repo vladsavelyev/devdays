9cafb071e8ecfa5454ab17d96b18e4744eb61b3c
jonyrock <ilms@live.ru>
2013-11-12 20:06:03 +0400

    url back again

--- devdays/devdays/urls.py
+++ devdays/devdays/urls.py
@@ -8,24 +8,24 @@ EVENT_PATTERN = r'(?P<month>\d{2})_(?P<year>\d{4})/?'
 
 urlpatterns = patterns(
     '',
-    (r'^/?$', 'devdays_app.views.index_view'),
-    (r'^openid/', include('django_openid_auth.urls')),
-    (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^/?$', 'devdays_app.views.index_view'),
+    url(r'^openid/', include('django_openid_auth.urls')),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     
-    (r'^event/' + EVENT_PATTERN, 'devdays_app.events.views.index'),
-    (r'^events/?', 'devdays_app.views.events_view'),
+    url(r'^event/' + EVENT_PATTERN, 'devdays_app.events.views.index'),
+    url(r'^events/?', 'devdays_app.views.events_view'),
     
-    (r'^idea/(?P<ideaId>\d+)/?', 'devdays_app.ideas.views.index'),
-    (r'^ideas/?', 'devdays_app.ideas.views.list_items'),
+    url(r'^idea/(?P<ideaId>\d+)/?', 'devdays_app.ideas.views.index'),
+    url(r'^ideas/?', 'devdays_app.ideas.views.list_items'),
     
-    (r'^project/(?P<id>.+)/?', 'devdays_app.views.project_view'),
+    url(r'^project/(?P<id>.+)/?', 'devdays_app.views.project_view'),
     
-    (r'^user/(?P<name>.+)/?', 'devdays_app.views.user_view'),
-    (r'^users/?', 'devdays_app.views.users_view'),
+    url(r'^user/(?P<name>.+)/?', 'devdays_app.views.user_view'),
+    url(r'^users/?', 'devdays_app.views.users_view'),
     
-    (r'^_ajax_new_idea/?', 'devdays_app.views.ajax_new_idea'),
+    url(r'^_ajax_new_idea/?', 'devdays_app.views.ajax_new_idea'),
     
-    (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += staticfiles_urlpatterns()
