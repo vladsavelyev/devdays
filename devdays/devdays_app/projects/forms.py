5e1b042b227bce46855acbe01e2d31dc834a6957
Na88ie <nast.mois@gmail.com>
2013-11-12 12:32:05 +0400

    add create_idea_view

---
+++ devdays/devdays_app/projects/forms.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'annie'
+from django.forms import forms, CharField, URLField, Textarea
+
+
+class CreateIdea(forms.Form):
+    name = CharField()
+    description = CharField(widget=Textarea, initial=u'Введите описание')
+    autor = CharField()
+    link = URLField(required=False)
+
+print(CreateIdea())
--- devdays/devdays_app/projects/views.py
+++ devdays/devdays_app/projects/views.py
@@ -1,16 +1,27 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
-
+from forms import CreateIdea
 from django.contrib.auth.decorators import login_required
 from sqlalchemy.event import Events
-from devdays_app.models import Project
+from devdays_app.models import Project, Idea
 
 
 def index(request, event):
     """
     Show all projects
     """
+    
     return render_to_response('projects/index.html')
 
 @login_required
-def new(request):    
-    return render_to_response('projects/new.html')
+def new(request):
+    if request.method == 'POST':
+        form = CreateIdea(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Idea.objects.create(**cd)
+        else:
+            form = CreateIdea()
+            return render_to_response('createidea.html', {'form': form})
+    return HttpResponse()
+
