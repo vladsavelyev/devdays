edd06d9b8287e0a9075b39fd998f8e7e6c75993c
vladsaveliev <vladsaveliev@me.com>
2013-11-14 10:48:58 +0400

    Deployment: ignore venv, import path

--- devdays/devdays/wsgi.py
+++ devdays/devdays/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/var/www/devdays/devdays')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devdays.settings")
 
 from django.core.wsgi import get_wsgi_application
